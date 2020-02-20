0bfcd867446aa331d55af8c2252b78d4f3b9d98c
rosi-souza <rosi.souza@live.com>
2020-02-20 10:51:17 -0300

    adicionando class view

---
+++ app/js/views/View.js
@@ -0,0 +1,11 @@
+class View {
+    constructor(seletor) {
+        this._elemento = document.querySelector(seletor);
+    }
+    update(model) {
+        this._elemento.innerHTML = this.template(model);
+    }
+    template(model) {
+        throw Error('É necessario implementar o  metodo template');
+    }
+}
--- app/ts/controllers/NegociacaoController.ts
+++ app/ts/controllers/NegociacaoController.ts
@@ -1,11 +1,12 @@
 class NegociacaoController {
 
-   private _inputData: HTMLInputElement;
-   private _inputQuantidade: HTMLInputElement;
-   private _inputValor: HTMLInputElement;
-   private _negociacoes = new Negociacoes();
-  private _negociacoesView = new NegociacoesView('#negociacoesView')
-
+  private _inputData: HTMLInputElement;
+  private _inputQuantidade: HTMLInputElement;
+  private _inputValor: HTMLInputElement;
+  private _negociacoes = new Negociacoes();
+  private _negociacoesView = new NegociacoesView('#negociacoesView');
+  private _messagemView = new MensagemView('#mensagemView');
+  
   constructor() {
     this._inputData = <HTMLInputElement>document.querySelector('#data');
     this._inputQuantidade = <HTMLInputElement>document.querySelector('#quantidade');
@@ -24,6 +25,7 @@ class NegociacaoController {
     );
     this._negociacoes.adiciona(negociacao)
     this._negociacoesView.update(this._negociacoes);
+    this._messagemView.update('Negociação enviada com sucesso!');
    
   }
 }
